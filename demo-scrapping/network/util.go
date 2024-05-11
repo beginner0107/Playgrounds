10678b6c071bd194271b595428da722c5d4f6386
Beginner <dkstmdwn17@naver.com>
2024-05-11 22:26:27 +0900

    범용성 있는 router util 함수 작성

---
+++ demo-scrapping/network/util.go
@@ -0,0 +1,52 @@
+package network
+
+import (
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
+// POST, GET, PUT, DELETE
+
+type API_REQUEST uint8
+
+const (
+	GET API_REQUEST = iota
+	POST
+	PUT
+	DELETE
+)
+
+type header struct {
+	result int    `json:"result"`
+	data   string `json:"data"`
+}
+
+type response struct {
+	*header
+	result interface{} `json:"result"`
+}
+
+// Router에서 사용가능한 범용성있는 유틸 함수
+
+func res(c *gin.Context, code int, res interface{}, data ...string) {
+	c.JSON(code, &response{
+		header: &header{result: code, data: strings.Join(data, " ,")},
+		result: res,
+	})
+}
+
+func (n *Network) register(path string, t API_REQUEST, h ...gin.HandlerFunc) gin.IRoutes {
+	switch t {
+	case GET:
+		return n.engin.GET(path, h...)
+	case POST:
+		return n.engin.POST(path, h...)
+	case PUT:
+		return n.engin.PUT(path, h...)
+	case DELETE:
+		return n.engin.DELETE(path, h...)
+
+	default:
+		return nil
+	}
+}
