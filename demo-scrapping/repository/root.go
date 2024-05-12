399666c0550bb741bbd6ed76277e79d015933ee6
Beginner <dkstmdwn17@naver.com>
2024-05-12 12:10:00 +0900

    View, ViewAll 구성

--- demo-scrapping/repository/root.go
+++ demo-scrapping/repository/root.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 	"demo-scrapping/config"
+	"demo-scrapping/types/schema"
 	"strings"
 )
 
@@ -12,6 +13,11 @@ type repository struct {
 }
 
 type RepositoryImpl interface {
+	Add(url, cardSelector, innerSelector string, tag []string) error
+	Update(url, cardSelector, innerSelector string, tag []string) error
+	Delete(url string) error
+	View(url string) (*schema.Admin, error)
+	ViewAll() ([]*schema.Admin, error)
 }
 
 const (
@@ -38,9 +44,57 @@ func (r *repository) Add(url, cardSelector, innerSelector string, tag []string) 
 	return err
 }
 
-func (r *repository) View(url string) (interface{}, error) {}
+func (r *repository) View(url string) (*schema.Admin, error) {
+	s := new(schema.Admin)
+	qs := query([]string{"SELECT * FROM", admin, "WHERE url = ?"})
 
-func (r *repository) ViewAll() ([]interface{}, error) {}
+	err := r.db.QueryRow(qs, url).Scan(
+		&s.ID,
+		&s.URL,
+		&s.CardSelector,
+		&s.InnerSelector,
+		&s.Tag,
+		&s.CreatedAt,
+		&s.UpdatedAt,
+	)
+
+	return s, err
+}
+
+func (r *repository) ViewAll() ([]*schema.Admin, error) {
+	qs := query([]string{"SELECT * FROM", admin})
+	if cursor, err := r.db.Query(qs); err != nil {
+		return nil, err
+	} else {
+		defer cursor.Close()
+
+		var result []*schema.Admin
+
+		for cursor.Next() {
+			s := new(schema.Admin)
+
+			if err = cursor.Scan(
+				&s.ID,
+				&s.URL,
+				&s.CardSelector,
+				&s.InnerSelector,
+				&s.Tag,
+				&s.CreatedAt,
+				&s.UpdatedAt,
+			); err != nil {
+				return nil, err
+			} else {
+				result = append(result, s)
+			}
+		}
+
+		if len(result) == 0 {
+			return []*schema.Admin{}, nil
+		} else {
+			return result, nil
+		}
+	}
+}
 
 func (r *repository) Update(url, cardSelector, innerSelector string, tag []string) error {
 	q := query([]string{"UPDATE", admin, "SET tag = ? cardSelector = ? innerSelector = ? WHERE url = ?"})
